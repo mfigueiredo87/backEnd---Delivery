3e004387084d5ac17807886d60136ac46711aa6d
mfigueiredo87 <figtch@gmail.com>
2024-08-14 00:43:34 +0100

    funcoes

--- app.py
+++ app.py
@@ -1,16 +1,55 @@
-print(""" 
+# bibliotecas
+import os 
+
+# funcao que lista o nome do programa
+def exibir_nome_do_programa():
+    print(""" 
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
 
 """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+# MENU
+def exibir_funcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
+# definir funcao para finalizar o programa
+def finalizar_app():
+    os.system('cls')
+    # os.system('clear') // no mac
+    print('Finalizando o app \n')
+    
 # input do usuario
-opcao_escolhida = input('Escolha uma opcao: ')
+opcao_escolhida = int(input('Escolha uma opcao: '))
+# outra forma de converter a string em inteiro
+# opcao_escolhida = int(opcao_escolhida)
 # print('voce escolheu a opcao', opcao_escolhida)
 # outra forma de passar a string
-print(f'voce escolheu a opcao {opcao_escolhida}')
+# print(f'voce escolheu a opcao {opcao_escolhida}')
+
+
+    # funcao para esoclher opcoes
+def escolher_opcao():
+    # verificar as opcoes
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida ==2:
+        print('Listar Restaurante')
+    elif opcao_escolhida ==3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+    
+    
+# funcao main que chama as funcoes existentes no codigo
+def main():
+    exibir_nome_do_programa()
+    exibir_funcoes()
+    escolher_opcao()
+    
+if __name__ == '__main__':
+    main()
+    
