8a74db49a605e5aa876269be231175f54c22b133
mfigueiredo87 <figtch@gmail.com>
2024-08-13 22:36:08 +0100

    primeiro

---
+++ app.py
@@ -0,0 +1,16 @@
+print(""" 
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+# input do usuario
+opcao_escolhida = input('Escolha uma opcao: ')
+# print('voce escolheu a opcao', opcao_escolhida)
+# outra forma de passar a string
+print(f'voce escolheu a opcao {opcao_escolhida}')
