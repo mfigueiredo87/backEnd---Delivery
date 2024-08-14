28beafc5c7923fb6d00716840fb93c11d02bd15c
mfigueiredo87 <figtch@gmail.com>
2024-08-14 11:08:09 +0100

    adicionando funcoes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # bibliotecas
 import os 
 
+# criar uma lista global
+restaurantes = ['Pizza','Art Doce','Vicky','Millenium']
+
 # funcao que lista o nome do programa
 def exibir_nome_do_programa():
     print(""" 
@@ -18,34 +21,63 @@ def exibir_funcoes():
 
 # definir funcao para finalizar o programa
 def finalizar_app():
-    os.system('cls')
-    # os.system('clear') // no mac
-    print('Finalizando o app \n')
+    exibir_subtitulos('Finalizando o aplicativo...')
+
     
-# input do usuario
-opcao_escolhida = int(input('Escolha uma opcao: '))
-# outra forma de converter a string em inteiro
-# opcao_escolhida = int(opcao_escolhida)
-# print('voce escolheu a opcao', opcao_escolhida)
-# outra forma de passar a string
-# print(f'voce escolheu a opcao {opcao_escolhida}')
+def voltar_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu.')
+    main()
 
-
+# para exibir o titulo e limpar a tela
+def exibir_subtitulos(texto):
+    os.system('cls')
+    print(texto)
+    print()
+    
+# se o utilizador digitar uma opcao invalida
+def opcao_invalida():
+    print('Opcao invalida!\n')
+    voltar_menu_principal()
+    
+    # cadastrar restaurante
+def cadastrar_novo_restaurante():
+    exibir_subtitulos('Cadastro de novos restaurantes')
+    nome = input('\nDigite o nome do restaurante a cadastrar: ')
+    restaurantes.append(nome)
+    print(f'O restaurante: {nome} foi cadastrado com sucesso.\n')
+    voltar_menu_principal()
+    
+def listar_restaurantes():
+    exibir_subtitulos('Listar restaurantes')
+    # estrutura de repeticao para exibir os restaurantes na tela
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    voltar_menu_principal()
+    
     # funcao para esoclher opcoes
 def escolher_opcao():
-    # verificar as opcoes
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida ==2:
-        print('Listar Restaurante')
-    elif opcao_escolhida ==3:
-        print('Ativar Restaurante')
-    else:
-        finalizar_app()
+    # usando try except 
+    try:
+        # input do usuario
+        opcao_escolhida = int(input('Escolha uma opcao: '))
+        # verificar as opcoes
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida ==2:
+            listar_restaurantes()
+        elif opcao_escolhida ==3:
+            print('Ativar Restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
     
     
 # funcao main que chama as funcoes existentes no codigo
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_funcoes()
     escolher_opcao()
